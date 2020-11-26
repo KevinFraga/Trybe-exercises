15f32a2181d7b7caf477059191736c63f78b05b1
Kevin Fraga <kevinciolac@gmail.com>
2020-11-26 17:51:12 -0300

    Add Exec. 2

--- bloco_4/dia_2/script.js
+++ bloco_4/dia_2/script.js
@@ -1,7 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-//Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 
-for(let index = 0; index < numbers.length; index += 1){
-    console.log(numbers[index]);
+for (let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index]);
 }
+
+// Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
+
+let somatorio = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  somatorio += numbers[index];
+}
+console.log(somatorio);
