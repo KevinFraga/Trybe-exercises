48ea97b0535051088b500299e329d7b6b926138d
Kevin Fraga <kevinciolac@gmail.com>
2020-11-26 17:55:53 -0300

    Add Exec. 4

--- bloco_4/dia_2/script.js
+++ bloco_4/dia_2/script.js
@@ -19,3 +19,11 @@ console.log(somatorio);
 let media;
 media = somatorio / numbers.length;
 console.log(media);
+
+// Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+if (media > 20) {
+  console.log("valor maior que 20");
+} else {
+  console.log("valor menor ou igual a 20");
+}
