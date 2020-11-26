abc746bdbbff0d3e681b02c3a5bfa984ad4efaf0
Kevin Fraga <kevinciolac@gmail.com>
2020-11-26 17:58:48 -0300

    Add Exec. 5

--- bloco_4/dia_2/script.js
+++ bloco_4/dia_2/script.js
@@ -27,3 +27,13 @@ if (media > 20) {
 } else {
   console.log("valor menor ou igual a 20");
 }
+
+// Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+let maior = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > maior) {
+    maior = numbers[index];
+  }
+}
+console.log(maior);
