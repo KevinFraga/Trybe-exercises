5c95ae7d43c11286d925c21893352d8ff8876f7b
Kevin Fraga <kevinciolac@gmail.com>
2020-11-26 18:16:03 -0300

    Add Exec. 8

--- bloco_4/dia_2/script.js
+++ bloco_4/dia_2/script.js
@@ -61,3 +61,11 @@ for (let index = 0; index < numbers.length; index += 1) {
   }
 }
 console.log(menor);
+
+// 8- Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
+
+let array = [];
+for (let index = 1; index < 26; index += 1) {
+  array.push(index);
+}
+console.log(array);
