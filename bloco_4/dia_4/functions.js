9d6a6ea22702c69f693e2e73d12369332a569487
Kevin Fraga <kevinciolac@gmail.com>
2020-11-30 20:47:55 -0300

    Parte II

--- bloco_4/dia_4/functions.js
+++ bloco_4/dia_4/functions.js
@@ -32,15 +32,86 @@ console.log(Maior([2, 3, 6, 7, 10, 1]));
 // 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 
 function Menor(array) {
-    let helper = array[0],
-      answer = 0;
-    for (let index in array) {
-      if (array[index] < helper) {
-        helper = array[index];
-        answer = index;
+  let helper = array[0],
+    answer = 0;
+  for (let index in array) {
+    if (array[index] < helper) {
+      helper = array[index];
+      answer = index;
+    }
+  }
+  return answer;
+}
+
+console.log(Menor([2, 4, 6, 7, 10, 0, -3]));
+
+// 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+let nome = ["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"];
+
+function longo(array) {
+  let answer = 0;
+  for (let index in array) {
+    if (array[index].length > array[answer].length) {
+      answer = index;
+    }
+  }
+  return array[answer];
+}
+
+console.log(longo(nome));
+
+// 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+function repete(array) {
+  let helper,
+    max = 0,
+    answer;
+  for (let index in array) {
+    helper = 0;
+    for (let counter in array) {
+      if (array[counter] === array[index]) {
+        helper += 1;
       }
     }
-    return answer;
+    if (helper > max) {
+      max = helper;
+      answer = array[index];
+    }
   }
+  return answer;
+}
 
-  console.log(Menor([2, 4, 6, 7, 10, 0, -3]));
+console.log(repete([2, 3, 2, 5, 8, 2, 3, 3, 3, 3]));
+
+// 6 - Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N.
+
+function somatorio(number) {
+  let answer = 0;
+  for (let index = 1; index <= number; index += 1) {
+    answer += index;
+  }
+  return answer;
+}
+
+console.log(somatorio(4));
+
+// 7 - Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word
+
+function verificaFimPalavra(word, ending) {
+  let answer = true,
+    helper = 0;
+  for (
+    let index = word.length - ending.length;
+    index < word.length;
+    index += 1
+  ) {
+    if (word[index] != ending[helper]) {
+      answer = false;
+    }
+    helper += 1;
+  }
+  return answer;
+}
+
+console.log(verificaFimPalavra("trybe", "be"));
