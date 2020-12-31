7f96ca70d746b3087988a09fb793d75eedd82875
Kevin Fraga <kevinciolac@gmail.com>
2020-12-31 13:33:05 -0300

    Ex. :eight:

--- bloco_5/dia_3/script.js
+++ bloco_5/dia_3/script.js
@@ -206,3 +206,19 @@ function addTask(task) {
 }
 
 addTask("Teste");
+
+/* Exercício 8:
+Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+*/
+
+function colorCode(color) {
+  let task = document.createElement('div');
+  let tasks = document.querySelector(".my-tasks");
+  task.classList.add('task');
+  task.style.backgroundColor = color;
+  tasks.appendChild(task);
+}
+
+colorCode("blue");
