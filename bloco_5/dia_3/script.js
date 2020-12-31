f38e9da7846ca5b3333e44d8cc4dfdbeaeb7fd03
Kevin Fraga <kevinciolac@gmail.com>
2020-12-31 14:36:10 -0300

    Ex. Bonus

--- bloco_5/dia_3/script.js
+++ bloco_5/dia_3/script.js
@@ -270,3 +270,32 @@ function taskColorSelection() {
 }
 
 taskColorSelection();
+
+/* Bônus:
+Vamos adicionar compromissos ao seu calendário? Implemente uma função que, ao digitar um compromisso na caixa de texto "COMPROMISSOS", adiciona o item à lista "MEUS COMPROMISSOS" ao clicar no botão "ADICIONAR".
+Se nenhum caractere for inserido no campo input , a função deve retornar um alert com uma mensagem de erro ao clicar em "ADICIONAR".
+Ao pressionar a tecla "enter" o evento também deverá ser disparado.
+*/
+
+function newTask() {
+  let task = document.getElementById('task-input').value;
+  if (task === '') {
+    return alert("Error");
+  }
+  addTask(task);
+}
+
+function enterKey(event) {
+  if (event.keyCode === 13) {
+    newTask();
+  }
+}
+
+function newTaskSetup() {
+  let input = document.getElementById('task-input');
+  let button = document.getElementById('btn-add');
+  input.addEventListener('keydown', enterKey);
+  button.addEventListener('click', newTask);
+}
+
+newTaskSetup();
