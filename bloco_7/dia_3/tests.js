b2ac30859ce4b4cf9e713bdd12455f78e1768e00
Kevin Fraga <kevinciolac@gmail.com>
2021-01-14 20:29:12 -0300

    :sunrise: 7.3 - Ex. :two:

--- bloco_7/dia_3/tests.js
+++ bloco_7/dia_3/tests.js
@@ -1,3 +1,11 @@
+/* 1) 
+A função sum(a, b) retorna a soma do parâmetro a com o b
+  Teste se o retorno de sum(4, 5) é 9
+  Teste se o retorno de sum(0, 0) é 0
+  Teste se a função sum lança um erro quando os parametros são 4 e "5" (string 5)
+  Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+*/
+
 const assert = require('assert');
 
 function sum(a, b) {
@@ -9,14 +17,6 @@ function sum(a, b) {
 }
 
 // implemente seus testes aqui
-
-/* 1) 
-A função sum(a, b) retorna a soma do parâmetro a com o b
-  Teste se o retorno de sum(4, 5) é 9
-  Teste se o retorno de sum(0, 0) é 0
-  Teste se a função sum lança um erro quando os parametros são 4 e "5" (string 5)
-  Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
-*/
 
 assert.strictEqual(sum(4, 5), 9);
 assert.strictEqual(sum(0, 0), 0);
@@ -33,3 +33,23 @@ A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse ar
   Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
 */
 
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [ 1, 2, 4 ]);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [ 1, 2, 3, 4 ]);
+
+const array = [1, 2, 3, 4, 5];
+const array2 = myRemove(array, 5);
+assert.deepStrictEqual(array, [1, 2, 3, 4, 5]);
+
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
