9dc923c890709c612cfcbd3a3047d14350005632
Kevin Fraga <kevinciolac@gmail.com>
2021-01-14 18:09:20 -0300

    :sunrise: 7.2 - Parte 2 - Ex. :one:

--- bloco_7/dia_2/exercise.js
+++ bloco_7/dia_2/exercise.js
@@ -1,3 +1,5 @@
+// Parte 1
+
 const order = {
   name: 'Rafael Andrade',
   phoneNumber: '11-98763-1416',
@@ -34,6 +36,11 @@ const order = {
   },
 };
 
+/*
+Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
+  Note que o parâmetro da função já está sendo passado na chamada da função.
+*/
+
 const customerInfo = (order) => {
   // Adicione abaixo as informações necessárias.
   const answer = `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}.`;
@@ -41,6 +48,12 @@ const customerInfo = (order) => {
 }
 
 customerInfo(order);
+
+/*
+Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de muzzarella, calabresa e Coca-Cola Zero é R$ 50,00."
+  Modifique o nome da pessoa compradora.
+  Modifique o valor total da compra para R$ 50,00.
+*/
 
 const orderModifier = (order) => {
   // Adicione abaixo as informações necessárias.
@@ -51,3 +64,35 @@ const orderModifier = (order) => {
 }
 
 orderModifier(order);
+
+// Parte 2
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+
+function addKey(object, key, value) {
+  object[key] = value;
+}
+
+addKey(lesson2, 'turno', 'manhã');
+
+console.log(lesson2);
