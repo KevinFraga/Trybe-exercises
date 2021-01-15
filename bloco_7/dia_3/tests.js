b643029be2ac0c478047d53d571f9e03e174e095
Kevin Fraga <kevinciolac@gmail.com>
2021-01-15 02:03:15 -0300

    :sunrise: 7.3 - Ex. :one::two:

--- bloco_7/dia_3/tests.js
+++ bloco_7/dia_3/tests.js
@@ -261,3 +261,20 @@ const parameter2 = 'Dayane';
 const result2 = 'D1y2n3';
 
 assert.deepStrictEqual(removeVowels(parameter2), result2);
+
+// 12) Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
+
+const greaterThanTen = (array) => {
+  const results = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] > 10) {
+      results.push(array[i]);
+    }
+  }
+  return results;
+};
+
+const parameter3 = [4, 10, 32, 9, 21];
+const result3 = [32, 21];
+
+assert.deepStrictEqual(greaterThanTen(parameter3), result3);
