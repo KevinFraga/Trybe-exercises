f13bc6d63b611bdc7c204b66a437fa2e754ab18b
Kevin Fraga <kevinciolac@gmail.com>
2021-01-15 01:59:12 -0300

    :sunrise: 7.3 - Ex. :one::one:

--- bloco_7/dia_3/tests.js
+++ bloco_7/dia_3/tests.js
@@ -231,3 +231,33 @@ const parameter1 = ['Irina', 'Ashleigh', 'Elsa'];
 const result1 = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
 
 assert.deepStrictEqual(greetPeople(parameter1), result1);
+
+// 11) Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
+
+const removeVowels = (word) => {
+  const characters = word.split('');
+  let results = '';
+  let helper = 0;
+
+  for (let i = 0; i < characters.length; i += 1) {
+    if (
+      characters[i] === 'a' ||
+      characters[i] === 'o' ||
+      characters[i] === 'i' ||
+      characters[i] === 'e' ||
+      characters[i] === 'u'
+    ) {
+      helper += 1;
+      results += `${helper}`;
+    } else {
+      results += `${characters[i]}`;
+    }
+  }
+  return results;
+};
+
+
+const parameter2 = 'Dayane';
+const result2 = 'D1y2n3';
+
+assert.deepStrictEqual(removeVowels(parameter2), result2);
