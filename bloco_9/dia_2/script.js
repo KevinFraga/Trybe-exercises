4b0f82bddec38ab652e7b614ce342bac35d7828e
Kevin Fraga <kevinciolac@gmail.com>
2021-01-28 19:30:18 -0300

    Ex. :one:

--- bloco_9/dia_2/script.js
+++ bloco_9/dia_2/script.js
@@ -1,7 +1,20 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
+
+// 1 - Como o primeiro exercício, vamos usar a função fetch , que vimos na aula ao vivo, para criar um site simples com um gerador de piadas ruins! . Como? Vamos fazer juntos!
+
 const fetchJoke = () => {
   // Adicionar lógica aqui!
+  const myObject = {
+  method: 'GET',
+  headers: { 'Accept': 'application/json' }
+  };
+  
+  // A partir daí, faça a piada aparecer no DOM da sua página!
+  const joke_container = document.getElementById('jokeContainer');
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(response => joke_container.innerText = response.joke);
 };
 
 window.onload = () => fetchJoke();
