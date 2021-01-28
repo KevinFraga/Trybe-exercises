64f4ecbfa916adb35aeb1d4eb12a881a0dab40cc
Kevin Fraga <kevinciolac@gmail.com>
2021-01-28 20:06:19 -0300

    Ex. :two:

--- bloco_9/dia_2/script.js
+++ bloco_9/dia_2/script.js
@@ -1,6 +1,5 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
-
 // 1 - Como o primeiro exercício, vamos usar a função fetch , que vimos na aula ao vivo, para criar um site simples com um gerador de piadas ruins! . Como? Vamos fazer juntos!
 
 const fetchJoke = () => {
@@ -16,5 +15,23 @@ const fetchJoke = () => {
     .then(response => response.json())
     .then(response => joke_container.innerText = response.joke);
 };
+
+/*
+2 - Agora, um passo para trás: vamos fazer, passo a passo, uma Promise . 
+Primeiramente, instancie uma Promise . Dentro dela, você deve produzir um array com dez números aleatórios de 1 a 50 e elevá-los todos ao quadrado. 
+Se a soma de todos esses elementos for inferior a 8000, a promise deve ser resolvida. 
+Caso contrário, ela deve ser rejeitada. Acresça um then e um catch à Promise com qualquer coisa dentro só para que o código funcione.
+*/
+
+const number_promise = new Promise((resolve, reject) => {
+  const numbers = [];
+  let sum;
+  for (let index = 0; index < 10; index += 1) numbers[index] = Math.ceil(Math.random() * 50);
+  sum = numbers.map(number => number * number).reduce((acc, cur) => acc + cur, 0);
+  if (sum < 8000) resolve(sum);
+  else reject(sum);
+})
+  .then(response => console.log(`A Promessa deu certo, a soma foi ${response}`))
+  .catch(response => console.log(`A promessa falhou, a soma foi ${response}`));
 
 window.onload = () => fetchJoke();
