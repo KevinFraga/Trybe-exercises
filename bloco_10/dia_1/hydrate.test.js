83a4674e0a73d65ee106f4e765e41db9b4939dde
Kevin Fraga <kevinciolac@gmail.com>
2021-02-18 19:27:41 -0300

    hydrate.test.js

---
+++ bloco_10/dia_1/hydrate.test.js
@@ -0,0 +1,26 @@
+const hydrate = (drinks) => {
+  let helper = 0;
+  [...drinks].forEach((character) => {
+    if (character >= 1 && character <= 9) helper += parseInt(character, 10);
+  });
+  if (helper === 1) return `${helper} copo de água`;
+  return `${helper} copos de água`;
+};
+
+// Implemente a função hydrate a partir dos testes abaixo. Experimente refatorar a função que você criou para o projeto Playground Function! É importante nunca alterar os testes ou as variáveis já escritas no código
+
+describe('Testa a função hydrate', () => {
+  it('Testa se a função hydrate é definida', () => {
+    expect(hydrate).toBeDefined();
+  });
+  it('Testa se hydrate é uma função', () => {
+    expect(typeof hydrate).toBe('function');
+  });
+  it('Ao receber uma string retorne a sugestão de quantos copos de água deve-se beber', () => {
+    expect(hydrate('1 cerveja')).toBe('1 copo de água');
+    expect(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho')).toBe('7 copos de água');
+    expect(hydrate('2 shots de tequila, 2 cervejas e 1 corote')).toBe('5 copos de água');
+    expect(hydrate('1 copo de catuaba, 1 cervejas e 1 copo de vinho')).toBe('3 copos de água');
+    expect(hydrate('4 caipirinhas e 2 cervejas')).toBe('6 copos de água');
+  });
+});
