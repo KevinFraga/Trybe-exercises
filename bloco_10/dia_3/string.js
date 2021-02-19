2c4aaca54fb31a0689390841ede7da5e1c1b2d40
Kevin Fraga <kevinciolac@gmail.com>
2021-02-19 18:37:24 -0300

    Ex. 4

---
+++ bloco_10/dia_3/string.js
@@ -0,0 +1,7 @@
+const uppperCase = (string) => string.toUpperCase();
+
+const firstLetter = (string) => string.slice(0, 0);
+
+const concat = (string1, string2) => `${string1}${string2}`;
+
+module.exports = { uppperCase, firstLetter, concat };
---
+++ bloco_10/dia_3/string.test.js
@@ -0,0 +1,25 @@
+const string = require('./string.js');
+
+/*
+Dentro de um mesmo arquivo, crie três funções.
+A primeira deve receber uma string e retorná-la em caixa alta.
+A segunda deve também receber uma string e retornar só a primeira letra.
+A terceira deve receber duas strings e concatená-las.
+Faça o mock do arquivo.
+Faça uma nova implementação para a primeira função, mas agora ela deve retornar a string em caixa baixa.
+Para a segunda função, uma nova implementação deve retornar a última letra de uma string.
+A terceira deve receber três strings e concatená-las.
+*/
+
+jest.mock('./string.js');
+
+test('mock implementations', () => {
+  string.uppperCase.mockImplementation((string) => string.toLowerCase());
+  string.firstLetter.mockImplementation((string) => string.slice(-1));
+  string.concat.mockImplementation(
+    (string1, string2, string3) => `${string1}${string2}${string3}`
+  );
+  expect(string.uppperCase('Teste')).toBe('teste');
+  expect(string.firstLetter('teste')).toBe('e');
+  expect(string.concat('a', 'b', 'c')).toBe('abc');
+});
