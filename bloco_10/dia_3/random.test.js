15180512dca72d6a6bd1c5ba7e31e1365dc097c0
Kevin Fraga <kevinciolac@gmail.com>
2021-02-19 16:55:48 -0300

    Ex. 2 e 3

--- bloco_10/dia_3/random.test.js
+++ bloco_10/dia_3/random.test.js
@@ -8,11 +8,36 @@ function random() {
   return Math.floor(Math.random() * 100);
 }
 
-test('testing random', () => {
+test('mocking random', () => {
   random = jest.fn().mockReturnValue(10);
   random();
   expect(random).toHaveBeenCalled();
   expect(random).toHaveBeenCalledTimes(1);
   expect(random()).toBe(10);
   expect(random).toHaveBeenCalledTimes(2);
+});
+
+/*
+Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo.
+Essa implementação deve ocorrer uma única vez. Faça os testes necessários.
+*/
+
+test('mock implementation once', () => {
+  random = jest.fn(() => 10).mockImplementationOnce((a, b) => a / b);
+  expect(random(15, 3)).toBe(5);
+  expect(random()).toBe(10);
+});
+
+/*
+Ainda com a mesma função do primeiro exercício, utilizando o mock, crie uma nova implementação que receba três parâmetros e retorne sua multiplicação.
+Após fazer os devidos testes para ela, resete sua implementação e crie uma nova, que receba um parâmetro e retorne seu dobro.
+Faça os testes necessários.
+*/
+
+test('multiple mock implementations', () => {
+  random = jest.fn().mockImplementation((a, b, c) => a * b * c);
+  expect(random(1, 2, 3)).toBe(6);
+
+  random.mockReset().mockImplementation((a) => 2 * a);
+  expect(random(2)).toBe(4);
 });
