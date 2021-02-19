399c443c9aaacecb62c007f3e756ed254d9bdae0
Kevin Fraga <kevinciolac@gmail.com>
2021-02-19 16:32:45 -0300

    Ex. 01

--- bloco_10/dia_3/random.test.js
+++ bloco_10/dia_3/random.test.js
@@ -4,5 +4,15 @@ Utilizando o mock, defina o retorno padrão como 10.
 Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada.
 */
 
-const random = () => Math.floor(Math.random() * 100);
+function random() {
+  return Math.floor(Math.random() * 100);
+}
 
+test('testing random', () => {
+  random = jest.fn().mockReturnValue(10);
+  random();
+  expect(random).toHaveBeenCalled();
+  expect(random).toHaveBeenCalledTimes(1);
+  expect(random()).toBe(10);
+  expect(random).toHaveBeenCalledTimes(2);
+});
