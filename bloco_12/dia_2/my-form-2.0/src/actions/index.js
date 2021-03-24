2a94d39863c56c3333c85b66d5e7a3b8db3bf8de
Kevin Ciolac de Fraga <72105025+KevinFraga@users.noreply.github.com>
2021-03-24 17:59:25 -0300

    Revert "from react to redux"

--- bloco_12/dia_2/my-form-2.0/src/actions/index.js
+++
@@ -1,4 +0,0 @@
-export const newAction = (state) => ({
-  type: 'UPDATE',
-  state,
-});
--- bloco_12/dia_2/my-form-2.0/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import formReducer from './formReducer';
-
-const rootReducers = combineReducers({
-  formReducer,
-});
-
-export default rootReducers;
